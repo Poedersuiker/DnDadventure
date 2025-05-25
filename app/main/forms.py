89d0e8a98335ad3c2524a36968cec7d97bf51249
Poedersuiker <poederbuik@hotmail.com>
2025-05-25 11:48:01 +0200

    Delete app directory

--- app/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-# This file can be used for forms specific to the 'main' blueprint.
-# Currently, no forms are defined here.
-# Example:
-# from wtforms import StringField, SubmitField
-# from wtforms.validators import DataRequired
-# class SomeMainForm(FlaskForm):
-#     some_field = StringField('Some Field', validators=[DataRequired()])
-#     submit = SubmitField('Submit')
